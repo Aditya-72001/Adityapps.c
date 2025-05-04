6821ef69297b7b4df0002dfdddd74ad0e3e8b9fc
Aditya-72001 <adityanawarkhele31@gmail.com>
2025-05-05 01:16:45 +0530

    Add files via upload

---
+++ leaveletter.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() 
+{
+    printf("To,\n");
+    printf("The Class Incharge,\n");
+    printf("Dr.Babasaheb Ambedkar Technological University,\n");
+    printf("Lonere.\n\n");
+
+    printf("Subject: Leave Application for 2 Days\n\n");
+
+    printf("Respected Sir,\n\n");
+    printf("I am writing to inform you that I would not be able to attend lectures on 5th and 6th May 2025 due to health issues. I kindly request you to grant me leave for these two days.\n\n");
+    printf("I assure you that I will cover the missed syllabus with the help of classmates.\n\n");
+
+    printf("Thank you for your understanding.\n\n");
+    printf("Yours sincerely,\n");
+    printf("Aditya Nawarkhele\n");
+    printf("EXTC(F.Y)\n");
+    printf("PRN NO:- 24030331372001\n");
+
+    return 0;
+}
